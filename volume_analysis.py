874fea09186835ead549321b715fd7a4a4d2cb76
hjh1161514 <hjh1161514@users.noreply.github.com>
2024-10-02 10:47:48 +0900

    [FEAT] 에러를 QMessageBox로 확인 가능하도록 구현

--- volume_analysis.py
+++ volume_analysis.py
@@ -7,6 +7,7 @@ from PyQt5.QtWidgets import QFileDialog, QMessageBox, QLineEdit, QListWidget, QT
     QVBoxLayout, QHBoxLayout, QProgressDialog
 from PyQt5.QtCore import QCoreApplication, Qt
 
+
 class VolumeAnalysisApp(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -102,7 +103,7 @@ class VolumeAnalysisApp(QtWidgets.QWidget):
             else:
                 return None
         except Exception as e:
-            print(f"Error processing file {file_path}: {e}")
+            QMessageBox.critical(f"Error processing file {file_path}: {e}")
             return None
 
     def process_folder(self, folder_path, progress_dialog):
@@ -174,7 +175,7 @@ class VolumeAnalysisApp(QtWidgets.QWidget):
 
     def load_files_in_folder(self, folder_path):
         self.file_list.clear()  # 기존 파일 목록을 초기화
-        self.table_widget.hide() # 분석 테이블 미노출
+        self.table_widget.hide()  # 분석 테이블 미노출
         allowed_extensions = {'.mp3', '.mp4'}  # 허용된 파일 확장자 목록
         files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))
                  and os.path.splitext(f)[1].lower() in allowed_extensions]  # mp3, mp4 파일만 포함
